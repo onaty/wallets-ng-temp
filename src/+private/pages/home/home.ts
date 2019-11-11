eb00d2d4e8eac730dd9dc34203f8d969d55807b8
Oluwatobi Onasanya <onatty98@gmail.com>
2019-11-11 02:10:46 +0100

    transaction page

--- src/+private/pages/home/home.ts
+++ src/+private/pages/home/home.ts
@@ -5,10 +5,63 @@ import { NavController } from 'ionic-angular';
   selector: 'page-home',
   templateUrl: 'home.html'
 })
-export class HomePage {
 
+export class HomePage {
+  amount = 4802030.00;
+  hiddenAmount = '***.**';
+  hideTextButton = 'Hide Balance';
+  showTextButton = 'show Balance';
+  hideAmountButton = true;
+  currency = "₦";
+  transactions = [
+    {
+      icon: "custom-transfer",
+      details: "Transfer to Elegboja Seun",
+      amount: 13000,
+      date: "Feb 18"
+    },
+    {
+      icon: "custom-request",
+      details: "Recieved from Elegboja Seun",
+      amount: 3650,
+      date: "Feb 18"
+    },
+    {
+      icon: "custom-airtime",
+      details: "MTN Airtime",
+      amount: 500,
+      date: "Feb 18"
+    },
+    {
+      icon: "custom-transfer",
+      details: "Transfer to Elegboja Seun",
+      amount: 13000,
+      date: "Feb 18"
+    },
+    {
+      icon: "custom-request",
+      details: "Recieved from Elegboja Seun",
+      amount: 3650,
+      date: "Feb 18"
+    },
+    {
+      icon: "custom-airtime",
+      details: "MTN Airtime",
+      amount: 500,
+      date: "Feb 18"
+    },
+  ];
   constructor(public navCtrl: NavController) {
 
   }
 
+  hideButton() {
+    this.hideAmountButton = !this.hideAmountButton;
+    if (this.hideAmountButton == true) {
+      this.hideTextButton = "Hide Balance"
+    }
+    if (this.hideAmountButton == false) {
+      this.hideTextButton = "Show Balance"
+    }
+  }
 }
